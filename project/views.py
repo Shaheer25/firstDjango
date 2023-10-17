fdbb70893d9c7d1caab215ffe759402f707d1249
shaheer <shaheer@logichive.in>
2023-10-17 15:23:04 +0530

    initial

--- project/views.py
+++ project/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return render(request,'project.html')
+    return render(request, 'project/project.html')
 
 def project(request,id):
-    return HttpResponse('This is Our one Page'+' '+str(id))
+    return render(request, 'project/single-project.html')
