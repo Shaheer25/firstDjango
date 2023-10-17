18688f40acda0dc194b7d40acb4b9d5f7ea5f199
shaheer <shaheer@logichive.in>
2023-10-17 19:38:27 +0530

    initial

--- project/views.py
+++ project/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList=[
+    {
+        "id":"1",
+        "title":"Website",
+        "description":"Full Functional Website"
+    },
+    {
+        "id":"2",
+        "title":"App",
+        "description":"Full Functional App"
+    },
+    {
+        "id":"3",
+        "title":"Software",
+        "description":"Full Functional Software"
+    }
+]
+
 def projects(request):
-    return render(request, 'project/project.html')
+    msg="You're in the HomePage"
+    number =12
+    context={"message":msg,"number":number,"project":projectList}
+    return render(request, 'project/project.html',context)
 
 def project(request,id):
-    return render(request, 'project/single-project.html')
+    projectObj=None
+    for i in projectList:
+        if i["id"] == id:
+            projectObj=i
+    context={"project":projectObj}
+    return render(request, 'project/single-project.html',context)
